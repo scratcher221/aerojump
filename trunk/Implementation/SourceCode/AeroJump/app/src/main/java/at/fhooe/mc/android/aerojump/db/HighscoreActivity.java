1d27c9409c281c196eb9e10179ceef203d45274f
blitzi97 <blitzi_97@hotmail.com>
2017-07-03 11:57:03 +0200

    load highscores updated

--- trunk/Implementation/SourceCode/AeroJump/app/src/main/java/at/fhooe/mc/android/aerojump/db/HighscoreActivity.java
+++ trunk/Implementation/SourceCode/AeroJump/app/src/main/java/at/fhooe/mc/android/aerojump/db/HighscoreActivity.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import at.fhooe.mc.android.aerojump.MainActivity;
 import at.fhooe.mc.android.aerojump.R;
@@ -20,12 +22,22 @@ public class HighscoreActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscores);
 
-        GetHighscoresThread dT = new GetHighscoresThread(this);
+        final GetHighscoresThread dT = new GetHighscoresThread(this);
         dT.start();
 
-
-
-        TextView tv = (TextView)findViewById(R.id.activity_highscores_content);
-        tv.setText(GetHighscoresThread.HIGHSCORES);
+        final TextView tv = (TextView) findViewById(R.id.activity_highscores_content);
+        final Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!dT.isAlive()) tv.setText(GetHighscoresThread.HIGHSCORES);
+                        else tv.setText("Loading Highscores...");
+                    }
+                });
+            }
+        }, 0, 100);
     }
 }
